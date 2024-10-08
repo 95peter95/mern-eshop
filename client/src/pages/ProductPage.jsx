f90a554dd76a611f35c005b656c73a6f05261b21
95peter95 <peterpauco1@gmail.com>
2024-10-09 00:01:17 +0200

    added server express config and updated fetching on frontend with axios

--- client/src/pages/ProductPage.jsx
+++ client/src/pages/ProductPage.jsx
@@ -1,12 +1,21 @@
+import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-import products from '../products';
 import {Row, Col, Image, ListGroup, Card, Button, ListGroupItem} from 'react-bootstrap';
 import Rating from '../components/rating';
 import { Link } from 'react-router-dom';
+import axios from 'axios';
 
 const ProductPage = (props) => {
+    const [product, setProduct] = useState({});
     const { id: productId } = useParams();
-    const product = products.find((p) => p._id === productId)
+
+    useEffect(() => {
+        const fetchProduct = async () => {
+            const { data } = await axios.get(`/api/products/${productId}`)
+            setProduct(data);
+        };
+        fetchProduct();
+    }, [productId])
     return (
     <>
     <Link className='btn btn-light my-3' to='/'>Go Back</Link>
