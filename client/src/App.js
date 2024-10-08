da3ea8f30b1353f27371e11f19b68b9e75978557
95peter95 <peterpauco1@gmail.com>
2024-10-08 23:08:35 +0200

    frontend changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,19 @@
-import React from 'react'
+import { Outlet } from 'react-router-dom';
+import { Container } from 'react-bootstrap';
+import Header from './components/header';
+import Footer from './components/footer';
 
 const App = (props) => {
   return (
-    <h1>
-      Welcome to Shop
-    </h1>
+    <>
+      <Header/>
+      <main className='py-3'>
+        <Container>
+          <Outlet/>
+        </Container>
+      </main>
+      <Footer/>
+    </>
   )
 }
 
